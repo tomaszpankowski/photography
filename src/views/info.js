8f15641a6376a6e9f7ba1ee91f0cf81ddee6660c
tomaszpankowski <pankowski.email@gmail.com>
2022-01-21 13:09:07 +0100

    modified routing

---
+++ src/views/info.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Info extends Component{
+    render(){
+        return(        
+            <Container fluid className="info-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Info;
