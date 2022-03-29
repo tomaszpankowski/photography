17e1ff3215e4662621e3c535578605cf0e16f545
tomaszpankowski <pankowski.email@gmail.com>
2022-03-29 14:58:07 +0200

    frontend

--- src/components/about-s1.js
+++ src/components/about-s1.js
@@ -6,25 +6,20 @@ import Col from "../../node_modules/react-bootstrap/Col";
 class AboutS1 extends Component{
     render(){
         return(    
-            <Container fluid className="d-flex bg-secondary minh-50vh align-items-center py-5 border-top ">
+            <Container fluid className="about-s1 d-flex bg-secondary minh-50vh align-items-center py-5 border-top ">
                 <Row className="mx-0 w-100">
-                    <Col xs={12} md={6} className="minh-25vh d-none d-md-block"></Col>
-                    <Col xs={12} md={6} className="minh-50vh d-flex align-items-center">
+                    <Col xs={12} md={7} className="minh-25vh d-none d-md-block"></Col>
+                    <Col xs={12} md={5} className="minh-50vh d-flex align-items-center">
                         <div className="w-100 text-center text-md-start pt-5 text-shadow">
-                            <h5 className="text-white">
-                                Lorem ipsum
-                            </h5>
-                            <h2 className="dispaly-6 fw-bold text-white-50 text-shadow">
+                            <h2 className="dispaly-6 fw-bold text-dark-50">
                                 Dolor sit amet         
                             </h2>
-                            <p className="text-white">
+                            <p className="text-dark lead">
                                 Sed do eiusmod tempor incididunt ut labore et dolore magna 
                                 aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
                                 ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                                 Duis aute irure dolor in reprehenderit in voluptate velit 
-                                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-                                occaecat cupidatat non proident, sunt in culpa qui officia 
-                                deserunt mollit anim id est laborum.      
+                                esse cillum dolore eu fugiat nulla pariatur
                             </p>
                         </div>
                     </Col>
--- src/components/courses-s1.js
+++ src/components/courses-s1.js
@@ -6,15 +6,15 @@ import Col from "../../node_modules/react-bootstrap/Col";
 class CoursesS1 extends Component{
     render(){
         return(    
-            <Container fluid className="d-flex minh-25vh align-items-center py-5 pt-5">
+            <Container fluid className="courses-s1 d-flex minh-25vh align-items-center py-5 pt-5">
                 <Row className="mx-0 w-100 mt-5">
                     <Col xs={{span:12,order:1}} md={{span:7,order:2}} className="minh-25vh"></Col>
                     <Col xs={{span:12,order:2}} md={{span:5,order:1}} className="minh-50vh d-flex align-items-center">
-                        <div className="w-100 text-center text-md-start">
-                            <h2 className="dispaly-6 fw-bold text-white-50 text-shadow">
+                        <div className="w-100 text-center text-md-start text-shadow">
+                            <h2 className="dispaly-6 fw-bold text-dark-50">
                                 Unde omnis iste
                             </h2>
-                            <p className="text-white">
+                            <p className="text-dark lead">
                                 Natus error sit voluptatem accusantium doloremque laudantium, 
                                 totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
                                 et quasi architecto beatae vitae dicta sunt explicabo.
